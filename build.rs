4854c0e7b99fa28609bf1f3b23acb1301dd755cd
stendler <git@stendler.de>
2022-06-05 02:58:18 +0200

    build: don't generate proto source by default
    only useful for debugging to check what is getting generated.
    
    No need to make any changes (except maybe some naming....)

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     tonic_build::configure()
         //.build_server(false)
-        .out_dir("src/generated")  // you can change the generated code's location
+        //.out_dir("src/generated")  // you can change the generated code's location
         .compile(&["proto/sensor.proto"], &["proto"])
         .unwrap();
 }
