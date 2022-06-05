9ad91e41d69f91477655f5992a9f1cabcad365d0
stendler <git@stendler.de>
2022-06-05 03:01:01 +0200

    server: implement hello world

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, server!");
+mod grpc;
+
+use tonic::{transport::Server, Request, Response, Status};
+
+use grpc::sensor_server::{Sensor, SensorServer as GrpcSensorServer};
+use grpc::{Event, EventResponse};
+
+
+#[derive(Debug, Default)]
+pub struct SensorServer {}
+
+#[tonic::async_trait]
+impl Sensor for SensorServer {
+    async fn put_event(&self, request: Request<Event>) -> Result<Response<EventResponse>, Status> {
+        println!("received an event: {:?}", request);
+        let reply = EventResponse {
+            status: String::from("OK"),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let server = SensorServer::default();
+
+    Server::builder()
+        .add_service(GrpcSensorServer::new(server))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
