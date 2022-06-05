76a56e83535aa2f3fa7d2c067f9e576307a5a855
stendler <git@stendler.de>
2022-06-05 03:38:07 +0200

    sensor: client hello world

--- src/sensor.rs
+++ src/sensor.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, sensor!");
+mod grpc;
+
+use grpc::sensor_client::SensorClient as GrpcSensorClient;
+use grpc::Event;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GrpcSensorClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(Event {
+        volcano_name: "Tonic".into(),
+        uuid_sensor: "asdf".into(),
+        uuid_datapoint: "fdsa".into(),
+        x: 0,
+        y: 0,
+        z: 0,
+        data_timestamp: 0,
+    });
+
+    let response = client.put_event(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
