a1d3a0d90ca7f8c7a84ed1fc95320b7b0256ffe3
stendler <git@stendler.de>
2022-06-08 00:50:33 +0200

    sensor: move run client into its own method

--- src/sensor.rs
+++ src/sensor.rs
@@ -6,8 +6,13 @@ use grpc::Event;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GrpcSensorClient::connect("http://[::1]:50051").await?;
+    let client = GrpcSensorClient::connect("http://[::1]:50051").await?;
+    run(client).await?;
+    Ok(())
+}
 
+
+async fn run(mut client:  GrpcSensorClient<tonic::transport::Channel>) -> Result<(), Box<dyn std::error::Error>> {
     let request = tonic::Request::new(Event {
         volcano_name: "Tonic".into(),
         uuid_sensor: "asdf".into(),
